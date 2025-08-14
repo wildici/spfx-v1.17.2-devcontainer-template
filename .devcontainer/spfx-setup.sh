e3f299dbfbee64205032f71ab49c2889f0835eae
Wildici Lins | Front-end Developer <wildici@hotmail.com>
2025-08-14 10:15:02 -0300

    Update spfx-setup.sh

--- .devcontainer/spfx-setup.sh
+++ .devcontainer/spfx-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install -g gulp-cli@2.3.0 yo@4.3.1 @microsoft/generator-sharepoint@1.17.2 spfx-fast-serve@3.0.6 spfx-fast-serve-helpers@1.17.0
+npm install -g gulp-cli@2.3.0 yo@4.3.1 @microsoft/generator-sharepoint@1.17.2
